64c5c1a617e501a3384400aa4fa7bf2293a6e5ca
kitamuraDev <masato1999824@icloud.com>
2023-12-08 11:19:33 +0900

    :sparkles: 複利計算（応用）を追加

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,16 @@
-type CompoundInterestFunc = (
-  annualAmount?: number,
-  annualRate?: number,
-  years?: number,
-) => number;
-
 /**
- * 積立投資の複利計算
+ * 積立投資の複利計算（基本）
  *
  * @param annualAmount 年間積立投資額
  * @param annualRate 年利（5% なら 1.05）
  * @param years 積立年数
  * @returns accumulateValue 累積額
  */
-const compoundInterest: CompoundInterestFunc = (
+const compoundInterestBasic = (
   annualAmount = 360000,
   annualRate = 1.05,
   years = 3,
-) => {
-  // for
-  // let accumulateValue = 0;
-  // for (let i = 1; i <= years; i++) {
-  //   accumulateValue = (accumulateValue + annualAmount) * annualRate;
-  // }
-
-  // Array.prototype.reduce
+): number => {
   const accumulateValue = Array.from({ length: years }).reduce<number>(
     (amount, _) => (amount + annualAmount) * annualRate,
     0,
@@ -32,4 +19,32 @@ const compoundInterest: CompoundInterestFunc = (
   return accumulateValue;
 };
 
-console.log(compoundInterest().toLocaleString()); // 1,191,645
+console.log(Math.trunc(compoundInterestBasic()).toLocaleString()); // 1,191,645
+
+console.log("====================");
+
+/**
+ * 積立投資の複利計算（応用）
+ *
+ * ユースケース
+ * 「n年はxx万円を積立てて、n年はxx万円を積立てる」ような、後から積立投資額を変更する場合の複利計算
+ *
+ * @param amounts 年間積立投資額
+ * @param years 積立年数
+ * @param rate 年利（5% なら 1.05）
+ * @returns accumulateValue 累積額
+ */
+const compoundInterestApply = (
+  amounts = [360000, 480000, 600000],
+  years = [5, 10, 5],
+  rate = 1.05,
+): number => {
+  const accumulateValue = amounts.reduce((acc, curr, i) => (
+    Array.from({ length: years[i] })
+      .reduce<number>((prev) => (prev + curr) * rate, acc)
+  ), 0);
+
+  return accumulateValue;
+};
+
+console.log(Math.trunc(compoundInterestApply()).toLocaleString()); // 15,914,059
